791a87a6a06a1ce4f263d39c49498d2f26382b43
SairaBeg <sairabeg.dev@gmail.com>
2023-12-29 22:07:18 -0500

    Methods separated into a different class
    Application finished

---
+++ Methods.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessing
+{
+    internal class Methods
+    {
+
+
+
+
+   
+
+   public  string checkNum(int guess, int correctNum, string enteredName)
+    {
+        string res;
+        if (guess == correctNum)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            return "That's correct! It was " + guess + ". You win, " + enteredName + ".";
+
+        }
+        else if (guess > correctNum)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            return "Your guess is greater than the correct number, try again.";
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            return "Your guess is lower than the correct number, try again";
+        }
+    }
+
+       public bool guessIsCorrect(int guess, int correctNum)
+        {
+            if (guess == correctNum)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+   public     int checkIfNum(string input, int guess)
+        {
+            bool valid = false;
+            while (!valid)
+            {
+                if (!int.TryParse(input, out guess))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please enter a valid number");
+                    Console.ResetColor();
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    valid = true;
+                }
+
+
+
+            }
+            return guess;
+
+        }
+    }
+ }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography.X509Certificates;
 
 
@@ -25,74 +26,99 @@ namespace NumberGuessing
             int correctNum = rnd.Next(1,11);
             int guess = 0;
             bool numberFound = false;
-            while (!numberFound) {
-                Console.ResetColor();
-                Console.WriteLine("Guess a number between 1 and 10");
-
-                string input = Console.ReadLine();
-                guess = checkIfNum(input);
-
-                Console.WriteLine("you guessed " + guess);
-                Console.WriteLine(checkNum(guess, correctNum, enteredName));
-                numberFound = guessIsCorrect(guess, correctNum);
-                
-            }
-
-            string checkNum(int guess,int correctNum, string enteredName)
+            bool playing = true;
+            Methods mth = new Methods();
+            while (playing)
             {
-                string res;
-                if (guess == correctNum)
+                while (!numberFound && playing)
                 {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    return "That's correct! It was " + guess + ". You win, " + enteredName + ".";
                     Console.ResetColor();
+                    Console.WriteLine("Guess a number between 1 and 10");
+
+                    string input = Console.ReadLine();
+                    guess = mth.checkIfNum(input, guess);
+
+                    Console.WriteLine("you guessed " + guess);
+                    Console.WriteLine(mth.checkNum(guess, correctNum, enteredName));
+                    numberFound = mth.guessIsCorrect(guess, correctNum);
+                    Console.ResetColor();
+
+
                 }
-                else if (guess > correctNum)
+                Console.WriteLine("Play again? [Y or N]");
+                string playAgain = Console.ReadLine().ToUpper();
+                if (playAgain == "Y")
                 {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    return "Your guess is greater than the correct number, try again.";
+                    numberFound = false;
+                    correctNum = rnd.Next(1, 11);
+                    guess = 0;
+                  
+                }
+                else if (playAgain == "N")
+                {
+                    playing = false;
+                    break;
                 }
                 else
                 {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    return "Your guess is lower than the correct number, try again";
+                    continue;
                 }
             }
+            //string checkNum(int guess,int correctNum, string enteredName)
+            //{
+            //    string res;
+            //    if (guess == correctNum)
+            //    {
+            //        Console.ForegroundColor = ConsoleColor.Green;
+            //        return "That's correct! It was " + guess + ". You win, " + enteredName + ".";
 
-            bool guessIsCorrect(int guess, int correctNum)
-            {
-                if (guess == correctNum)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            int checkIfNum(string input)
-            {
-                bool valid = false;
-                while (!valid)
-                {
-                    if (!int.TryParse(input, out guess))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Please enter a valid number");
-                        Console.ResetColor();
-                        input = Console.ReadLine();
-                    }
-                    else
-                    {
-                        valid = true;
-                    }
-                    
-                       
-                    
-                }
-                return guess;
+            //    }
+            //    else if (guess > correctNum)
+            //    {
+            //        Console.ForegroundColor = ConsoleColor.Yellow;
+            //        return "Your guess is greater than the correct number, try again.";
+            //    }
+            //    else
+            //    {
+            //        Console.ForegroundColor = ConsoleColor.Yellow;
+            //        return "Your guess is lower than the correct number, try again";
+            //    }
+            //}
 
-            }
+            //bool guessIsCorrect(int guess, int correctNum)
+            //{
+            //    if (guess == correctNum)
+            //    {
+            //        return true;
+            //    }
+            //    else
+            //    {
+            //        return false;
+            //    }
+            //}
+            //int checkIfNum(string input)
+            //{
+            //    bool valid = false;
+            //    while (!valid)
+            //    {
+            //        if (!int.TryParse(input, out guess))
+            //        {
+            //            Console.ForegroundColor = ConsoleColor.Red;
+            //            Console.WriteLine("Please enter a valid number");
+            //            Console.ResetColor();
+            //            input = Console.ReadLine();
+            //        }
+            //        else
+            //        {
+            //            valid = true;
+            //        }
+
+
+
+            //    }
+            //    return guess;
+
+            //}
 
         }
     }
